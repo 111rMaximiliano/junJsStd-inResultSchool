e4bb203232ac9613e525448110d062882ec22efe
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-05-30 00:15:27 +0400

    task 3/4 syntax

---
+++ car.js
@@ -0,0 +1,16 @@
+const carName = "Porsche";
+let maxCarSpeed = 350;
+let carOwner = "I";
+
+maxCarSpeed = 355;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = "He";
+console.log("У машины изменился владелец", carOwner);
+maxCarSpeed = 360;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = "She";
+console.log("У машины изменился владелец", carOwner);
+maxCarSpeed = 370;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = "We";
+console.log("У машины изменился владелец", carOwner);
