0b96fc93c107a5950836407bf346f6d3fe27a866
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-05-30 11:18:27 +0400

    t4 type

---
+++ index.js
@@ -0,0 +1,39 @@
+const age = 25;
+const name = "Maks";
+const brain = true;
+const skill = null;
+let futureSkill;
+const obj = { city: "Samara", start: 2022 };
+const symb = Symbol("id");
+const cash = 2356789n;
+
+console.log(Number(age));
+console.log(Boolean(age));
+console.log(String(age));
+
+console.log(Number(name));
+console.log(Boolean(name));
+console.log(String(name));
+
+console.log(Number(brain));
+console.log(Boolean(brain));
+console.log(String(brain));
+
+console.log(Number(skill));
+console.log(Boolean(skill));
+console.log(String(skill));
+
+console.log(Number(futureSkill));
+console.log(Boolean(futureSkill));
+console.log(String(futureSkill));
+
+console.log(Number(obj));
+console.log(Boolean(obj));
+console.log(String(obj));
+
+console.log(Boolean(symb));
+console.log(String(symb));
+
+console.log(Number(cash));
+console.log(Boolean(cash));
+console.log(String(cash));
