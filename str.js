965c0755a237f5485dcca4f2ded601d5159eff0e
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-05-31 23:03:55 +0400

    t5-t4 str

---
+++ str.js
@@ -0,0 +1,10 @@
+let userName = prompt("Как вас зовут?");
+
+userName = userName.toLowerCase();
+userName = userName.trim();
+
+let userAge = prompt("Сколько вам лет?");
+
+userAge = Number(userAge);
+
+alert(`Вас зовут ${userName} и вам ${userAge}`);
