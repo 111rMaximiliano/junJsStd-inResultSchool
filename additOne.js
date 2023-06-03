2d8550a0670c9295b106c7ff1355d6b3e11a71ae
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-06-03 20:37:56 +0400

    Update additOne.js

--- additOne.js
+++ additOne.js
@@ -2,7 +2,7 @@ let javaScriptDescription =
   "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript. JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений.";
 javaScriptDescription = javaScriptDescription.slice(1, 114);
 javaScriptDescription = javaScriptDescription.toUpperCase();
-//javaScriptDescription = javaScriptDescription.translitCyrToLat() как провести транслитерацию так и не понял, плагин не срабатывает, написал коллегам в дискорд
+//javaScriptDescription = javaScriptDescription.translitCyrToLat() как провести транслитерацию так и не понял, плагин не срабатывает, написал коллегам в дискорд, ответа пока не поучил(за 5+ часов)
 javaScriptDescription = javaScriptDescription.trim();
 javaScriptDescription =
   javaScriptDescription +
