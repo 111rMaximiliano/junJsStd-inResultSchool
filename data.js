f7fae98047c47d9e54de22ae69a365c28c8dd582
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-06-16 09:49:11 +0400

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-const getDaysBeforeBirthday = () => {
-  let nextBirthdayDate = new Date("September 11, 2023");
-  let restOf = new Date("June 16, 2023");
-  let difference = nextBirthdayDate.getTime() - restOf.getTime();
-  console.log(Math.round(difference / 1000 / 60 / 60 / 24));
-};
-getDaysBeforeBirthday();
