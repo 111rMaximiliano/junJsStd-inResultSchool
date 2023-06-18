0dcdb9be363a860524c25ab52883b33fef9ad2a1
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-06-19 00:56:33 +0400

    Update arr.js

--- arr.js
+++ arr.js
@@ -7,3 +7,16 @@ for (let i = 0; i < 3; i++) {
   }
 }
 console.log(matrix);
+
+
+///100x100
+
+let matrix = [];
+
+for (let i = 0; i < 100; i++) {
+  matrix[i] = [];
+  for (let j = 0; j < 100; j++) {
+    matrix[i].push(j + 1);
+  }
+}
+console.log(matrix);
