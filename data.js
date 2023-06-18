08a0441ed32c16a4424d13eb774b648b8818b56e
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-06-18 21:47:48 +0400

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,17 @@
 const getDateFormat = (date, separator = ".") => {
   let result = "";
+  let zero = result.padStart(1, "0");
   if (date.getDate() < 9) {
-    result = result + "0";
+    result = result + zero;
   }
   result = result + date.getDate() + separator;
   if (date.getMonth() < 9) {
-    result = result + "0";
+    result = result + zero;
   }
   result = result + (date.getMonth() + 1) + separator;
   result = result + date.getFullYear();
   return result;
 };
 
-let date = new Date();
+let date = new Date("9.5.2023");
 console.log(getDateFormat(date));
