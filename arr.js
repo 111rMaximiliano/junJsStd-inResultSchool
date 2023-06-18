049d485b20700a135af768864132e04dfd2075a9
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-06-19 00:51:32 +0400

    Update arr.js

--- arr.js
+++ arr.js
@@ -1,50 +1,70 @@
 const goals = [8, 1, 1, 3, 2, -1, 5];
+
 //1
+function goalsMax(goals) {
+  return Math.max.apply(null, goals);
+}
+console.log(goalsMax(goals));
+
+const maxGoals = goals.findIndex((item) => {
+  return item === goalsMax(goals);
+});
+console.log(maxGoals);
 alert(
-  `Самый результативный матч был под номером ${goals.indexOf(
-    8
-  )}. В нём было забито ${goals[0]} гол(ов).`
+  `Самый результативный матч был под номером ${maxGoals}. В нём было забито ${goalsMax(
+    goals
+  )} голов.`
 );
 
 //2
+
+const minGoal = goals.filter((number, index, goals) => {
+  // console.log(index);
+  return goals.indexOf(number) == 1;
+});
+console.log(minGoal);
+
 alert(
   `Самые нерезультативные матчи были под номерами ${goals.indexOf(
     1
-  )}, ${goals.lastIndexOf(1)}. В каждом из них было забито по ${
-    goals[1]
-  } мячу(а).`
+  )} и ${goals.lastIndexOf(1)} . В каждом из них было забито ${minGoal}`
 );
 
 //3
-const sum = goals.reduce((acc, item) => {
-  return (acc += item);
-}, 0);
-alert(`Общее количество голов за сезон равно ${sum}`);
 
-//4
-function loseGame(goals) {
-  for (let i = 5; i < goals.length; i++) {
-    if (goals[i] < 1) {
-      alert("Были автоматические поражения: да");
-    } else {
-      alert("Были автоматические поражения: нет");
-    }
+function numbersOfGoals(goals) {
+  let sum = 0;
+  for (let i = 0; i < goals.length; i++) {
+    sum += goals[i];
   }
+  return sum;
 }
 
-loseGame(goals);
+alert(`Общее количество голов за сезон равно ${numbersOfGoals(goals)}`);
+
+//4
+function goalsMin(goals) {
+  return Math.min.apply(null, goals);
+}
+console.log(goalsMin(goals));
+
+if (goalsMin(goals) <= 0) {
+  alert(`Были автоматические поражения: Да`);
+} else {
+  alert(`Были автоматические поражения: Нет`);
+}
 
 //5
+
 const getAverage = (goals) => {
   const sum1 = goals.reduce((acc, number) => acc + number, 0);
   const length = goals.length;
   return sum1 / length;
 };
-const sumGetAverage = getAverage(goals);
+const sumGetAverage = getAverage(goals).toFixed();
 alert(`Среднее количество голов за матч равно ${sumGetAverage}`);
 
 //6
-
 const sortGoals = goals.sort((a, b) => {
   return a - b;
 });
