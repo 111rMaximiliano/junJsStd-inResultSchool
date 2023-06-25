58d8ed87d887096de7434bd2be4d179c5c7055be
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-06-25 23:12:43 +0400

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,15 @@
+function getKiller(suspectInfo, deadPeople) {
+  for (let key of Object.keys(suspectInfo)) {
+    if (deadPeople.every((e) => suspectInfo[key].includes(e))) return key;
+  }
+}
+console.log(
+  getKiller(
+    {
+      James: ["Jacob", "Bill", "Lucas"],
+      Johnny: ["David", "Kyle", "Lucas"],
+      Peter: ["Lucy", "Kyle"],
+    },
+    ["Lucas", "Bill"]
+  )
+);
