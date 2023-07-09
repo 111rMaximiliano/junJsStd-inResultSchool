7363f930cce686237b79890e85e91b13fb88acee
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-07-10 01:03:35 +0400

    Update obj.js

--- obj.js
+++ obj.js
@@ -1,3 +1,37 @@
+// NEW CODE
+const groceries = {
+  "73Wakv": {
+    name: "Orange Juice",
+    price: 1.5,
+    discount: 10,
+  },
+  "5L3db9": {
+    name: "Chocolate",
+    price: 2,
+    discount: 0,
+  },
+};
+
+const shoppingBag = [
+  { productId: "73Wakv", count: 25 },
+  { productId: "5L3db9", count: 23 },
+];
+
+function getTotalPriceOfShoppingBag(shoppingBag) {
+  shoppingBag[0].price = groceries["73Wakv"].price;
+  shoppingBag[0].discount = groceries["73Wakv"].discount;
+  shoppingBag[1].price = groceries["5L3db9"].price;
+  let sum = (
+    (shoppingBag[0].price * shoppingBag[0].count) / shoppingBag[0].discount +
+    shoppingBag[1].price * shoppingBag[1].count
+  ).toFixed();
+  console.log(sum);
+}
+getTotalPriceOfShoppingBag(shoppingBag);
+
+// OLD CODE
+
+/*
 const groceries = {
   "73Wakv": {
     name: "Orange Juice",
@@ -32,21 +66,6 @@ let shoppingBag = [
   { productId: "6L2db1", count: 5 },
 ];
 
-/*let shoppingBag = [
-  { productId: "73Wakv", count: 2 },
-  { productId: "5L3db9", count: 3 },
-  { productId: "6L2db1", count: 5 },
-];
-console.log(shoppingBag[0]);*/
 const totalPrice = getTotalPriceOfShoppingBag(shoppingBag);
 console.log(totalPrice);
-
-/*console.log(
-  (groceries["73Wakv"].price * shoppingBag[0].count) /
-    groceries["73Wakv"].discount
-);
-
-console.log(groceries["73Wakv"].price);
-console.log(shoppingBag[0].count);
-console.log(groceries["73Wakv"].discount);
 */
